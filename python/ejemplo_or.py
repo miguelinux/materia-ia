8870bb03b98219e71112f8a6d26af47f1c0baea2
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2025-11-07 18:44:18 -0600

    ejemplo or

---
+++ python/ejemplo_or.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+
+from perceptron import Perceptron
+
+def escalon(valor):
+    if valor >= 0:
+        return 1
+    return 0
+
+def main():
+    red = Perceptron()
+    red.set_funcion_activacion(escalon)
+    entradas = [ [0, 0],
+                 [0, 1],
+                 [1, 0],
+                 [1, 1] ]
+
+    salida = [0,
+              1,
+              1,
+              1]
+
+    red.entrenar(entradas, salida)
+    res = red.predecir(entradas)
+    print("*" * 60)
+    print(res, "=", salida)
+
+if __name__ == "__main__":
+    main()
